2ef8304ccd7b744d1f071bfaea0c702b849b454b
IToriginal <gitshineit@gmail.com>
2023-07-07 15:01:56 +0900

    Education(나도코딩-자바 기본편): 05 - for문
    - 짝수, 홀수, 거꾸로 출력 그리고 반복문의 합을 구하는 조건의 범위를 연습

--- java-basic/java_basic/java-basic/src/main/java/org/example/ch04/_05_for.java
+++ java-basic/java_basic/java-basic/src/main/java/org/example/ch04/_05_for.java
@@ -13,5 +13,30 @@ public class _05_for {
                 System.out.println("어서오세요 " + i + "번째 손님 originalit에 오신 것을 환영합니다");
             }
         }
+
+        // 짝수만 출력: 02468
+        for (int i = 0; i < 10; i += 2) {
+            System.out.print(i);
+        }
+        System.out.println();
+
+        // 홀수만 출력: 13579
+        for (int i = 1; i < 10; i += 2) {
+            System.out.print(i);
+        }
+        System.out.println();
+
+        // 거꾸로 숫자: 54321
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i);
+        }
+        System.out.println();
+
+        // 1부터 10까지의 수의 합: 55
+        int sumI = 0;
+        for (int i = 1; i <= 10; i++) {
+            sumI += i;
+        }
+        System.out.println(sumI);
     }
 }
