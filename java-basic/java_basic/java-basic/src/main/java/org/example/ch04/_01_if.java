9acb157963bcd0be13855d09fa3088e5b454304f
IToriginal <gitshineit@gmail.com>
2023-07-07 13:39:44 +0900

    Education(나도코딩-자바 기본편): 01 - if문
    - 조건식 2개

--- java-basic/java_basic/java-basic/src/main/java/org/example/ch04/_01_if.java
+++ java-basic/java_basic/java-basic/src/main/java/org/example/ch04/_01_if.java
@@ -3,7 +3,7 @@ package org.example.ch04;
 public class _01_if {
     public static void main(String[] args) {
         // 조건문 if
-        int hour = 15; // 오전 10시를 의미
+        int hour = 15; // 오후 3시를 의미
 
         // if 문 내에 하나의 문장만 존재할 때는 { } 를 생략할 수 있다.
         if (hour > 14)
@@ -13,6 +13,14 @@ public class _01_if {
         if (hour <= 14) {
             System.out.println("ICE 아메리카노 + 1");
             System.out.println("샷추가");
+        }
+        System.out.println("커피 주문이 완료되었습니다.");
+
+        // 오후 2시 이전, 모닝 커피를 마시지 않은 경우의 조건문
+        hour = 10;
+        boolean moningCoffee = false; // 모닝커피 정보
+        if (hour < 14 && moningCoffee == false) {
+            System.out.println("ICE 아메리카노 + 1");
         }
         System.out.println("커피 주문이 완료되었습니다.");
     }
