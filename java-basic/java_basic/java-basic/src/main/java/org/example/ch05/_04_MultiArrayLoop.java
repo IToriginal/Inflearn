0d27c88eb9268d11092b614d5220bd17904603c3
IToriginal <gitshineit@gmail.com>
2023-07-08 21:30:03 +0900

    Education(나도코딩-자바 기본편): ch05.4 - 다차원 배열 초기화

--- java-basic/java_basic/java-basic/src/main/java/org/example/ch05/_04_MultiArrayLoop.java
+++ java-basic/java_basic/java-basic/src/main/java/org/example/ch05/_04_MultiArrayLoop.java
@@ -30,5 +30,27 @@ public class _04_MultiArrayLoop {
             }
             System.out.println();
         }
+
+        System.out.println("------------- 구분선 -------------");
+        // 세로 크기 10 x 가로 크기 15에 해당하는 영화관 좌석
+        String[][] seats3 = new String[10][15];
+        String[] eng = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        for (int i = 0; i < seats3.length; i++) { // 세로
+            for (int j = 0; j < seats3[i].length; j++) {
+                seats3[i][j] = eng[i] + (j + 1);
+            }
+        }
+
+        // 표 구매 (H9 H10)
+        seats3[7][8] = "__";
+        seats3[7][9] = "___";
+
+        // 영화관 좌석 번호 확인
+        for (int i = 0; i < seats3.length; i++) {
+            for (int j = 0; j < seats3[i].length; j++) {
+                System.out.print(seats3[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
