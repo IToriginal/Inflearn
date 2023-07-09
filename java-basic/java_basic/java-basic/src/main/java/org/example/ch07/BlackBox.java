4238e5c65304d6f291a9880ee83189b25bd344cc
IToriginal <gitshineit@gmail.com>
2023-07-09 17:55:31 +0900

    Education(나도코딩-자바 기본편): ch07.2 - 인스턴스 변수 (필드)

--- java-basic/java_basic/java-basic/src/main/java/org/example/ch07/BlackBox.java
+++ java-basic/java_basic/java-basic/src/main/java/org/example/ch07/BlackBox.java
@@ -1,4 +1,12 @@
 package org.example.ch07;
 
 public class BlackBox {
+    // 인스턴스 변수(필드) 생성
+    String modelName; // 모델명
+
+    String resolution; // 해상도
+
+    int price; // 가격
+
+    String color; // 색상
 }
