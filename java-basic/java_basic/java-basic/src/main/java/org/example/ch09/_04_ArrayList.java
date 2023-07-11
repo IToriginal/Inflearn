5ce914f133c5d7f1857d2b837f35dfad1ebe58cd
IToriginal <gitshineit@gmail.com>
2023-07-11 18:53:05 +0900

    Education(나도코딩-자바 기본편): ch09.4 - ArrayList
    - set()
    - indexOf()
    - contains()
    - isEmpty()
    - Collections.sort()

--- java-basic/java_basic/java-basic/src/main/java/org/example/ch09/_04_ArrayList.java
+++ java-basic/java_basic/java-basic/src/main/java/org/example/ch09/_04_ArrayList.java
@@ -1,6 +1,8 @@
 package org.example.ch09;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class _04_ArrayList {
     public static void main(String[] args) {
@@ -39,6 +41,53 @@ public class _04_ArrayList {
         System.out.println("---------- 구분선 ----------");
 
         // 순회
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("---------- 구분선 ----------");
+
+        // 데이터 변경
+        System.out.println("인덱스 0 변경 전: " + list.get(0));
+        list.set(0, "디미트리");
+        System.out.println("인덱스 0 변경 후: " + list.get(0));
+
+        System.out.println("---------- 구분선 ----------");
+
+        // 데이터 조회
+        System.out.println(list.indexOf("티구안")); // 1
+
+        System.out.println("---------- 구분선 ----------");
+
+        // 리스트에 포함되어 있는지?
+        if (list.contains("티구안")) {
+            System.out.println("선착순 성공");
+        } else {
+            System.out.println("선착순 실패");
+        }
+
+        System.out.println("---------- 구분선 ----------");
+
+        // 전체 삭제
+        list.clear();
+        if (list.isEmpty()) {
+            System.out.println("데이터를 비웠습니다.");
+            System.out.println("list 내의 데이터는 "+ list.size());
+        }
+
+        System.out.println("---------- 구분선 ----------");
+
+        // 데이터 다시 추가
+        list.add("마이콜");
+        list.add("티구안");
+        list.add("피치");
+        list.add("람보");
+        list.add("카티치");
+
+        System.out.println(list);
+
+        // 정렬
+        Collections.sort(list);
         for (String s : list) {
             System.out.println(s);
         }
