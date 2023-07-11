b3eb5c973b17caff20f9d8b4537dd51bf407218f
IToriginal <gitshineit@gmail.com>
2023-07-11 18:43:17 +0900

    Education(나도코딩-자바 기본편): ch09.4 - ArrayList
    - add()
    - get()
    - remove()
    - size()

---
+++ java-basic/java_basic/java-basic/src/main/java/org/example/ch09/_04_ArrayList.java
@@ -0,0 +1,46 @@
+package org.example.ch09;
+
+import java.util.ArrayList;
+
+public class _04_ArrayList {
+    public static void main(String[] args) {
+        // 컬랙션 프레임워크: 많은 데이터를 효과적으로 관리하기 위한 클래스들의 모음
+        // List, Set, Map
+        ArrayList<String> list = new ArrayList<>();
+
+        // 데이터 추가
+        list.add("마이콜");
+        list.add("티구안");
+        list.add("피치");
+        list.add("람보");
+        list.add("카티치");
+
+        // 데이터 조회(index)
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+        System.out.println(list.get(3));
+        System.out.println(list.get(4));
+
+        System.out.println("---------- 구분선 ----------");
+
+        // 삭제 (람보 이사)
+        System.out.println("신청 학생 수 (이사 전): " + list.size()); // 5
+        list.remove("람보");
+        System.out.println("신청 학생 수 (이사 후): " + list.size()); // 4
+        System.out.println(list.get(3));
+
+        System.out.println("---------- 구분선 ----------");
+
+        System.out.println("남은 학생 수 (제외 전): " + list.size()); // 4
+        list.remove(list.size() - 1); // 마지막 데이터를 삭제
+        System.out.println("남은 학생 수 (제외 후): " + list.size()); // 3
+
+        System.out.println("---------- 구분선 ----------");
+
+        // 순회
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
